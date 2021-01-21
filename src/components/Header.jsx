ae6a3990ebe1cc6180f57aef5166c60656c659d3
fiixed <fiixed@gmail.com>
2021-01-21 13:08:33 -0500

    add links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,44 @@
 import React, { Component } from 'react'
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link, 
+    Redirect
+  
+  } from 'react-router-dom';
+
 export default class Header extends Component {
     render() {
         return (
-            <>
+            <Router>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Header</a>
+                <Link className="navbar-brand" to="/">Header</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">home <span className="sr-only">(current)</span></a>
+                            <Link className="nav-link" to="/">home <span className="sr-only">(current)</span></Link>
                         </li>
                         <span className="navbar-text">
                             -
                         </span>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">feed</a>
+                            <Link className="nav-link" to="/feed">feed</Link>
                         </li>
                         <span className="navbar-text">
                             -
                         </span>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">faq</a>
+                            <Link className="nav-link" to="/faq">faq</Link>
                         </li>
                     </ul>
                 </div>
             </nav>
-            </>
+            </Router>
         )
     }
 }
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,4 +1,15 @@
 import React, { Component } from 'react'
+import Feed from './Feed';
+import Faq from './Faq';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link, 
+    Redirect
+  
+  } from 'react-router-dom';
 
 export default class Page extends Component {
     render() {
